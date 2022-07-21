76f57fbed36631e9fd3e60e5b74d25bd00b8b4d2
Nii <bnstephens001@gmail.com>
2022-07-21 06:46:31 +0000

    Completed Assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,39 @@
 import React from "react";
+import Links from "./Links"
 
-function About() {
-  return (
-    <div id="about">
+function About(props) {
+  
+
+    //Conditional Rendering Has To Be Employed To Show the Presences of the <p> tag
+    
+    let presenceBio = (
+      <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{props.bio} <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" /></p>
+      {/* add your <Links /> component here */}
+      <Links github = {props.links.github} linkedin = {props.links.linkedin} />
+    </div>
+
+    );
+
+    let absentBio = (
+      <div id="about">
+      <h2>About Me</h2>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links github = {props.links.github} linkedin = {props.links.linkedin} />
     </div>
-  );
-}
+    )
+
+    //Statement to Render the passing of the p tag
+    const validateBio = props.bio
+    if (validateBio){
+      return presenceBio
+    }
+    else{
+      return absentBio
+    }
+    }
+    
 
 export default About;
